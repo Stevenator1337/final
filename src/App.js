6d9019a1727b97d0bb5eaf3c152fe4e316e7ff07
Stevenator1337 <70933933+Stevenator1337@users.noreply.github.com>
2022-05-22 16:10:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import LinkContainer from './components/LinkContainer'
 import './App.css';
+
+const body = document.querySelector('body');
+const changer = document.querySelector('section');
+  let color = '#';
+ body.addEventListener('click',() => {
+ 
+  color += Math.random().toString(16).slice(2,8);
+changer.style.backgroundColor = color;
+color = "#";
+ })
+ 
   function App() {
   return (
-    <div className="App">
-      <LinkContainer />
     
+    <div className="App">
+      
+      <LinkContainer />
+     
+      <section>
+   <button >Click Me</button>
+
+  </section>
+
+        
+         
+      
+
+  
     </div>
+    
   )
 }
+ 
+
 
 export default App
